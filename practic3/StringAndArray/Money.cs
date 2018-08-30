fc54dfb0a712d7a4bc35594a80e7a2caf8968a88
BarakpaevOlzhas <40425093+BarakpaevOlzhas@users.noreply.github.com>
2018-08-30 18:32:49 +0600

    Add files via upload

---
+++ practic3/StringAndArray/Money.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAndArray
+{
+    class Money
+    {
+        private double _nominalValue;
+        private int _count;
+
+        public Money()
+        {
+
+        }
+
+        public Money(double nominal_value, int count)
+        {
+            SetNominalValue(nominal_value);
+            SetCount(count);
+        }
+
+        public double GetNominalValue()
+        {
+            return this._nominalValue;
+        }
+
+        public void SetNominalValue(double nominalValue)
+        {
+            this._nominalValue = nominalValue;
+        }
+
+        public int GetCount()
+        {
+            return this._count;
+        }
+
+        public void SetCount(int count)
+        {
+            this._count = count;
+        }
+
+        public bool WillYouHaveEnoughMoney(double sum)
+        {
+            return (_nominalValue * _count) >= sum;
+        }
+
+        public int HowMuch(int sum)
+        {         
+            double amountOfMoney = _nominalValue * _count;
+
+            return sum / (int)amountOfMoney;
+        }
+
+        public double AmountOfMoney()
+        {
+            return _nominalValue * _count;
+        }
+
+    }
+}
---
+++ practic3/StringAndArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAndArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double bufDouble;
+            int bufInt;
+            Money money = new Money(10,2000);
+
+            Console.WriteLine("Введите номинал");
+            double.TryParse(Console.ReadLine(),out bufDouble);
+            money.SetNominalValue(bufDouble);
+
+            Console.WriteLine("Введите номинал");
+            int.TryParse(Console.ReadLine(), out bufInt);
+            money.SetCount(bufInt);
+
+            Console.WriteLine($"\n Ваш баланс: {money.AmountOfMoney()}");
+
+            
+
+            Console.WriteLine($"хватает ли вам на новый дом за 12тг: {money.WillYouHaveEnoughMoney(12)}");
+
+            Console.ReadLine();
+        }
+    }
+}
