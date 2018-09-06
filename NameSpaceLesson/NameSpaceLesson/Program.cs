52dcde291f670fafb21860f16f0c5b770e13f30c
BarakpaevOlzhas <40425093+BarakpaevOlzhas@users.noreply.github.com>
2018-09-06 18:30:27 +0600

    Add files via upload

---
+++ NameSpaceLesson/NameSpaceLesson/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace NameSpaceLesson
+{    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumM sum = new SumM();
+
+            sum.DivisionOfTwoNumbers(2,0);
+
+            ReadLine();
+
+        }
+    }
+}
---
+++ NameSpaceLesson/NameSpaceLesson/TryCatch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSpaceLesson
+{
+    class SumM
+    {
+        public double DivisionOfNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+
+        public void DivisionOfTwoNumbers(int firstNumber, int secondNumber)
+        {
+            try
+            {
+              DivisionOfNumbers(firstNumber, secondNumber);
+                
+            }
+            catch (ArithmeticException argumentException)
+            {
+                Console.WriteLine(argumentException);
+            }
+        }
+    }
+}
